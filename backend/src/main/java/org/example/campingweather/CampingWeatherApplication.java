a33c8ad6079736a3e3734aca9c9534485fb4e7f1
GabrielP2017 <zx78410@gmail.com>
2025-06-30 20:55:46 +0900

    first

---
+++ backend/src/main/java/org/example/campingweather/CampingWeatherApplication.java
@@ -0,0 +1,13 @@
+package org.example.campingweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingWeatherApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/example/campingweather/CampingWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.campingweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
