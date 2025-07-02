b33d7f502d87af212cbf75a47f3db2017c047648
GabrielP2017 <zx78410@gmail.com>
2025-07-03 03:19:22 +0900

    6
    6

--- backend/src/test/java/org/example/campingweather/CampingWeatherApplicationTests.java
+++ backend/src/test/java/org/example/campingweather/CampingWeatherApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.campingweather;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")   // application-test.yml을 사용하도록 지정
 class CampingWeatherApplicationTests {
 
     @Test
