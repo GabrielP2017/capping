bc27bd31c218cf5e88398fe4897870df8e93b536
GabrielP2017 <zx78410@gmail.com>
2025-07-08 22:05:18 +0900

    Revert "라스트"
    This reverts commit f340feefae1c556c731d6f4593ef81ad6b4e38e2.

--- backend/src/test/java/org/example/campingweather/CampingWeatherApplicationTests.java
+++ backend/src/test/java/org/example/campingweather/CampingWeatherApplicationTests.java
@@ -1,43 +1,15 @@
 package org.example.campingweather;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+import org.springframework.test.context.ActiveProfiles;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@Testcontainers
+@ActiveProfiles("test")   // application-test.yml을 사용하도록 지정
 class CampingWeatherApplicationTests {
-
-    // ① 테스트 전용 Postgres 컨테이너 정의
-    @Container
-    static PostgreSQLContainer<?> postgres =
-        new PostgreSQLContainer<>(DockerImageName.parse("postgis/postgis:15-3.4").asCompatibleSubstituteFor("postgres"))
-        .withDatabaseName("testdb")
-        .withUsername("postgres")
-        .withPassword("postgres");
-
-    // ② Spring DataSource·Flyway 프로퍼티 동적 주입
-    @DynamicPropertySource
-    static void props(DynamicPropertyRegistry r) {
-        r.add("spring.datasource.url", postgres::getJdbcUrl);
-        r.add("spring.datasource.username", postgres::getUsername);
-        r.add("spring.datasource.password", postgres::getPassword);
-
-        r.add("spring.flyway.url", postgres::getJdbcUrl);
-        r.add("spring.flyway.user", postgres::getUsername);
-        r.add("spring.flyway.password", postgres::getPassword);
-    }
 
     @Test
     void contextLoads() {
-        // 빈 로딩 + Flyway 마이그레이션 + JPA 스캔이 모두 성공하면 통과
     }
+
 }
